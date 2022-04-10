39b2ca9513939af9a9019ddc02f566b263ce1d6a
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-10 12:12:49 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char W;
+
+	W = 0;
+	while (W < 9)
+	{
+		putchar(W);
+	W++;
+	}
+	putchar('\n');
+
+	return (0);
+}
