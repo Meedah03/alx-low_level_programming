3de91c598d9c1928fd0e4a5c7a8563dba8acf915
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-10 13:25:35 -0700

    Print Z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Description: main - use putchar to print numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char U;
+
+	U = '0';
+
+	while (U <= '9')
+	{
+		putchar(U);
+		U++;
+	}
+	putchar('\n');
+
+	return (0);
+}
