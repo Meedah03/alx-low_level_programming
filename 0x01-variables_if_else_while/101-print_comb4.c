a3058658e3ec8211649f322228284ab5d4e1c01f
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-10 17:39:56 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * Description: main - entry
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; ++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 9 && b == 9)
+
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
