00dea68517770a52ccd5897cdecbdda989b11e68
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-11 14:31:15 -0700

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * Description: main - standard error
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
