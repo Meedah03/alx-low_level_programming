61ad0e5470884ffe0c2f3b736a79aada9625fae6
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-10 17:16:07 -0700

    99

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,18 +11,14 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
-		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
-
-			if (a == 9 && b == 9)
-				continue;
+		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
 
 			putchar(',');
 			putchar(' ');
 			}
-	}
+	putchar('\n');
 
 
 	return (0);
