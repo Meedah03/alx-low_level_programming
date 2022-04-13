1f63afe57a4842ed1149877183434371f231eae8
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-13 16:50:27 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: _isalpha - _isalpha(int c)
+ * @c: alphabets
+ * Return: 0 for lowercase, otherwise 1
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
