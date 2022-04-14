e19ef3c7d3bfc9ae7782f65aff3c75540a0faab5
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-14 15:30:56 -0700

    work

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,11 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+	
 	if (a > b && b > c)
 	{
 		largest = a;
@@ -22,7 +27,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-		largest = c
+		largest = c;
 	}
 
 	printf("%d is the largest number\n", largest);
