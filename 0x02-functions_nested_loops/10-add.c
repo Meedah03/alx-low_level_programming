c0768f91e89a3437650dfa032f398f8095444ba0
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-13 18:44:09 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Description:add - add 2 int
+ * @int: sum
+ * Return: result
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+	char c;
+
+	c = a + b;
+
+	_putchar(c + '0');
+	return (c);
+}
