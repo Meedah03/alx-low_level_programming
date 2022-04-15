c2901a4a394f1a3293d59e74362af918718e87aa
Meedah03 <101396194+Meedah03@users.noreply.github.com>
2022-04-15 01:01:43 +0100

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-void print_times_table(int n);
-
-#endif
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
