a2e4e854002e3dd8ca33edd4a97a40306ed75ace
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-20 16:51:32 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the integers of a and b
+ * @a: integer 1
+ * @b: integer 2
+ *
+ */
+
+void swap_int(int *a, int*b)
+{
+	int p = *a;
+
+		*a = *b;
+		*b = p;
+}
