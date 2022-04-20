85264c50baba29ce014c3cf41bdc9a8ccec3f739
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-20 15:55:38 -0700

    Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - reset to 98
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *n;
+
+	n = 402;
+	n = &n;
+	_putchar(n);
+	_putchar('\n');
+
+	*n = 98;
+	_putchar(n);
+	_putchar('\n');
+
+	return (0);
+}
