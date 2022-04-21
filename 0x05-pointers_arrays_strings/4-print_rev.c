02e7478e5d22c113e44d4d1855d885c5693dd0db
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-20 17:52:38 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - rint string in reverse
+ * @s: sring
+ *
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	whle (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
