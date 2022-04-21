e9dee4c613c696d5a2a302097a5e9020234e9915
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-20 17:15:45 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "main.h"
-
 /**
  * _strlen - length of string
  * @s: string
+ *
+ * Return: The length of string
  */
 
 int _strlen(char *s)
 {
 	int i;
 
-	i = strlen(s);
+	i = strlen(char *s);
+	return (i);
 }
