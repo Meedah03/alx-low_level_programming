fd4073b65a1ffa8a230a6b291d00e8a87e98b08c
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-21 20:33:17 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,13 +2,12 @@
 
 /**
  * print_rev - rint string in reverse
- * @s: sring
+ * @s: string
  *
  * Return: string in reverse
  */
 
 void print_rev(char *s)
-void _putchar (char)
 {
 	int i = 0;
 	while (s[i] != 0)
