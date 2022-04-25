7c69a7f44df129445aa9f16999b62ddc2bdeb4e9
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-24 17:47:04 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half
+ * @str: string
+ * Return 0
+ */
+void puts_half(char *str)
+{
+	char B[i];
+	int i;
+	int l;
+	int m;
+
+	l = strlen(str[i]);
+	m = (l / 2);
+		for (i = m; B = 0 && i <= l; i++, B++)
+			_putchar(B[i]);
+}
