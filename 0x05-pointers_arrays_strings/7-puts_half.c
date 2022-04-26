8c2727c391b4fa73b50ae0239d3c6587c45e63a9
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-26 16:32:29 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,23 @@
 void puts_half(char *str)
 {
 	int i;
-	int l;
-
-	l = i / 2;
-	for (i = *l; str[i] >= l; i++)
+		
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		i = i / 2;
+	}
+	else
+	{
+		i = (i + 1) / 2
+	}
+	while (str[i] != '\0')
+	{
+		i++;
 		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
