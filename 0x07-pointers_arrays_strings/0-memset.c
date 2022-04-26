62501345f55aa542e184eedd55ebc7f584e5972d
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-26 15:50:25 -0700

    Memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - function that reset memory
+ *
+ * @s: memory area to return
+ * @b: constant bytes
+ * @n: size of bytes
+ * Return: Nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+
+		*(s + i)n = b;
+		i += 1;
+	}
+	return (s);
+}
