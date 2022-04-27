59208a484eea1e050632be72c1b0ff37ee818336
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-27 06:39:31 -0700

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverse arrays
+ * @a: var
+ * @n: int
+ */
+void reverse_array(int *a, int n)
+{
+	n = 0;
+	while (n != 0)
+	{
+		n++;
+	}
+	while (n > 0)
+	{
+		n--;
+	}
+}
