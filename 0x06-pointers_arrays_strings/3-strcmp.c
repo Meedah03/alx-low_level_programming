088580f5712689c91110bd76d4af1abc8d98ad54
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-27 06:23:49 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,11 +4,13 @@
  * _strcmp - compare strings
  * @s1: str
  * @s2: str
- * Return: 0
+ * Return: i
  */
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int i;
 
-	return (0);
+	i = strcmp(s1, s2);
+
+	return (i);
 }
