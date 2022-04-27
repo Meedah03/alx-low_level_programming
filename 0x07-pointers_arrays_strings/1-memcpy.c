2606b31e335258af2ebc977ad6e402345bd93c77
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-26 17:43:58 -0700

    new

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,15 @@
  * @dest: character
  * @src: source
  * @n: int
+ * Return nothing
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*dest[i] = *src[n]
+	{
+		*dest[n] = *src[n];
+	}
 	return (dest);
 }
