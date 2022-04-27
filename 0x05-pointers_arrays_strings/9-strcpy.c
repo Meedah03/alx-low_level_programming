40cee131a3f26171ef8ccd84404d3909fd423ff5
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-27 04:53:27 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy string
+ * @dest: string
+ * @src: string
+ * Return 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+	strcpy(dest, src);
+
+	return (dest);
+}
