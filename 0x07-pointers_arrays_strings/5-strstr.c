cb4402ba66034b0c99ba667dd27a3d7e0aae5b1f
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-27 22:15:23 -0700

    Strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - locate substring
+ * @haystack: char
+ * @needle: char
+ * Return: i
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char i = strstr(haystack, needle);
+
+	return (i);
+}
