3f23b7c830586d475359a4c57b1251b440041a38
Meedah03 <meedat2003@gmaeedat2003@gmail.com>
2022-04-27 21:46:27 -0700

    STRSPN

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,14 +2,13 @@
 #include <string.h>
 /**
  * _strchr - locates caracter string
- * @f: char
+ * @s: char
+ * @c: char
  * Return: f
  */
 char *_strchr(char *s, char c)
 {
-	char *f;
+	char *f = strchr(s, c);
 
-	f = strchr(s, c);
-
-	return (f); 
+	return (f);
 }
